997dfc982b3875b71753708d3d7db13f9c88d698
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2018-02-27 22:48:48 +0100

    Kalkulator-1

---
+++ KalkulatorTest.java
@@ -0,0 +1,20 @@
+package Kalkulator;
+
+import java.awt.EventQueue;
+
+public class KalkulatorTest extends Kalkulator {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Kalkulator window = new Kalkulator();
+					window.getFrameKalkulatorStandardowy().setVisible(true);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}

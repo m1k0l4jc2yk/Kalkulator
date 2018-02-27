404ac688e3f7c3d7dc6a6960d1289b592a11325a
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2018-02-27 22:51:55 +0100

    Kalkulator-1

--- Kalkulator.java
+++ Kalkulator.java
@@ -66,4 +66,58 @@ public class Kalkulator extends Wykres {
         textField.setHorizontalAlignment(SwingConstants.RIGHT);
         getFrameKalkulatorStandardowy().getContentPane().add(textField);
         textField.setColumns(10);
+//		Rz�d 1 ------------------------------------------
+        JButton btnBackSpace = new JButton("\uF0E7");
+        btnBackSpace.setBounds(10, 60, 50, 50);
+        btnBackSpace.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String backspace = null;
+                if (textField.getText().length() > 0) {
+                    StringBuilder strB = new StringBuilder(textField.getText());
+                    strB.deleteCharAt(textField.getText().length() - 1);
+                    backspace = strB.toString();
+                    textField.setText(backspace);
+                }
+            }
+        });
+        btnBackSpace.setFont(new Font("Wingdings", Font.BOLD, 14));
+        getFrameKalkulatorStandardowy().getContentPane().add(btnBackSpace);
+
+        JButton btnClear = new JButton("C");
+        btnClear.setBounds(65, 60, 50, 50);
+        btnClear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textField.setText(null);
+            }
+        });
+        btnClear.setFont(new Font("Arial", Font.BOLD, 18));
+        getFrameKalkulatorStandardowy().getContentPane().add(btnClear);
+
+        JButton btnProcent = new JButton("%");
+        btnProcent.setBounds(120, 60, 50, 50);
+        btnProcent.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                double ops = Double.parseDouble(String.valueOf(textField.getText()));
+                ops = firstnumber * ops / 100;
+                textField.setText(String.valueOf(ops));
+                ops = firstnumber;
+            }
+        });
+        btnProcent.setFont(new Font("Arial", Font.BOLD, 16));
+        getFrameKalkulatorStandardowy().getContentPane().add(btnProcent);
+
+        JButton btnPlus = new JButton("+");
+        btnPlus.setBounds(175, 60, 50, 50);
+        btnPlus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                firstnumber = Double.parseDouble(textField.getText());
+                textField.setText("");
+                operations = "+";
+
+            }
+        });
+        btnPlus.setFont(new Font("Arial", Font.BOLD, 18));
+        getFrameKalkulatorStandardowy().getContentPane().add(btnPlus);
     }
